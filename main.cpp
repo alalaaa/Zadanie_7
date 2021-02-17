b9cdcc0c384b337d655a95e6502cb86f34e35377
alalaaa <62750005+alalaaa@users.noreply.github.com>
2021-02-17 09:40:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,49 +1,49 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 vector <int> scal(vector <int> a, vector <int> b) 
 {
-    vector <int> tablica;
+    vector <int> tb;
     
     for(!a.empty() && !b.empty())
 	{
         if(a[a.size()-1]>b[b.size()-1]) 
 		{
-            tablica.push_back(a[a.size()-1]);
+            tb.push_back(a[a.size()-1]);
             a.pop_back();
         }else
 		{
-            tablica.push_back(b[b.size()-1]);
+            tb.push_back(b[b.size()-1]);
             b.pop_back();
         }
     }
-    swap(tablica[i], tablica[tablica.size()-1-i]);
-    return tablica;
+    swap(tb[i], tb[tb.size()-1-i]);
+    return tb;
 }
 
-vector <int> uporzadkuj(vector <int> tablica) 
+vector <int> uporzadkuj(vector <int> tb) 
 {
     int k;
     vector <int> a;
     vector <int> b;
 
-    if(tablica.size()==1)
+    if(tb.size()==1)
     {
-        return tablica;
-    	k = tablica.size()/2;
+        return tb;
+    	k = tb.size()/2;
     }
     	
     for(int i=0; i<k; i++)
     {
-        a.push_back(tablica[i]);
+        a.push_back(tb[i]);
     	uporzadkuj(a);
     }
     	
     for(int i=k; i<k*2; i++)
     {
-    	b.push_back(tablica[i]);
+    	b.push_back(tb[i]);
     	uparzadkuj(b);
     	return scal(a, b);
     }
